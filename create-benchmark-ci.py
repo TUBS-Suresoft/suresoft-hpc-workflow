ae8b423d7e81ddf56c39e482b712a3702c362a77
Sören Peters <dev.peters@icloud.com>
2023-01-11 16:29:11 +0000

    Add dynamic pipeline for hpc benchmark.

---
+++ create-benchmark-ci.py
@@ -0,0 +1,30 @@
+hpcRocketjobs = ["rocket-mpich", "rocket-native", "rocket-openmpi", "rocket-mpich-bind"]
+
+
+benchmark_ci_file = """
+.benchmark:
+  stage: benchmark
+  image: python:3.10
+  before_script:
+    - pip install hpc-rocket==0.3.1
+"""
+
+for hpcRocketjob in hpcRocketjobs:
+    benchmark_ci_file += f"""
+benchmark:{hpcRocketjob}
+  extends: .benchmark
+
+  script:
+    - hpc-rocket launch --watch {hpcRocketjob}.yml
+    - cat results/laplace.out
+
+  artifacts:
+    expire_in: 1 hrs
+    paths:
+      - results/
+"""
+
+
+
+with open("benchmark-gitlab-ci.yml", "w") as file:
+    file.write(benchmark_ci_file)
