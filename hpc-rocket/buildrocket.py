692e2bdb1be2a1de9ad985d604e979002817ac21
Sven Marcus <s.marcus@outlook.de>
2023-01-11 18:28:47 +0100

    Add script to build rocket.yml

---
+++ hpc-rocket/buildrocket.py
@@ -0,0 +1,96 @@
+from pathlib import Path
+from dataclasses import asdict, dataclass
+from jinja2 import Template
+
+
+CONTAINER_DIR = Path("Containers")
+CONTAINER_BASENAME = "rockylinux9"
+
+JOB_DIR = Path("slurmjobs")
+
+ROCKET_TEMPLATE_PATH = Path("hpc-rocket/rocket.yml.template")
+ROCKET_TEMPLATE = Template(ROCKET_TEMPLATE_PATH.read_text())
+
+DEF_FILES = CONTAINER_DIR.glob("*.def")
+IMAGES = [def_file.with_suffix(".sif") for def_file in DEF_FILES]
+
+VARIANT_NAMES = [image.stem.removeprefix(CONTAINER_BASENAME + "-") for image in IMAGES]
+IMAGE_BY_VARIANT = dict(zip(VARIANT_NAMES, IMAGES))
+
+
+@dataclass
+class Rocket:
+    copy_instructions: list[tuple[str, str]]
+    collect_instructions: list[tuple[str, str]]
+    jobfile: str
+
+
+def collect_variants(job_dir: Path, variant: str) -> list[Path]:
+    variant_job_files = job_dir.glob(f"{variant}*.job")
+    return list(variant_job_files)
+
+
+def collect_jobs_by_variant() -> dict[str, list[Path]]:
+    jobs_by_variant: dict[str, list[Path]] = {}
+    for variant in VARIANT_NAMES:
+        jobs_by_variant[variant] = collect_variants(JOB_DIR, variant)
+
+    jobs_by_variant["native"] = collect_variants(JOB_DIR, "native")
+    return jobs_by_variant
+
+
+def native_copy_instructions(remote_dir: str) -> list[tuple[str, str]]:
+    if variant == "native":
+        return [("laplace2d", remote_dir + "/laplace")]
+
+    return []
+
+
+def rocket_from_variant(variant: str, jobfile: Path) -> Rocket:
+    remote_jobfile = jobfile.stem + "/laplace.job"
+    remote_output = jobfile.stem + "/" + jobfile.stem + ".out"
+    remote_results = jobfile.stem + "/results"
+
+    copy_instructions = build_copy_instructions(variant, jobfile, remote_jobfile)
+
+    return Rocket(
+        copy_instructions=copy_instructions,
+        collect_instructions=[
+            (remote_output, "results/"),
+            (remote_results, "results/"),
+        ],
+        jobfile=remote_jobfile,
+    )
+
+
+def build_copy_instructions(
+    variant: str, jobfile: Path, remote_jobfile: str
+) -> list[tuple[str, str]]:
+    copy_instructions = [(str(jobfile), remote_jobfile)]
+    copy_instructions.extend(image_copy_instructions(variant, jobfile))
+    copy_instructions.extend(native_copy_instructions(jobfile.stem))
+    return copy_instructions
+
+
+def image_copy_instructions(variant: str, jobfile: Path) -> list[tuple[str, str]]:
+    if variant in IMAGE_BY_VARIANT:
+        remote_image = jobfile.stem + "/" + IMAGE_BY_VARIANT[variant].name
+        local_image = str(IMAGE_BY_VARIANT[variant])
+        return [(local_image, remote_image)]
+
+    return []
+
+
+def save_rocket_file(variant: str, jobfile: Path) -> None:
+    rendered = ROCKET_TEMPLATE.render(**asdict(rocket_from_variant(variant, jobfile)))
+
+    ROCKET_TEMPLATE_PATH.with_name("rocket-" + jobfile.stem + ".yml").write_text(
+        rendered
+    )
+
+
+if __name__ == "__main__":
+    jobs_by_variant = collect_jobs_by_variant()
+    for variant, jobfiles in jobs_by_variant.items():
+        for jobfile in jobfiles:
+            save_rocket_file(variant, jobfile)
--- slurmjobs/buildjob.py
+++ slurmjobs/buildjob.py
@@ -1,30 +1,46 @@
 from dataclasses import asdict, dataclass
+from jinja2 import Template
 
 
-job_content: str
+JOB_CONTENT: str
 with open("slurmjobs/laplace.job.template") as job_template:
-    job_content = job_template.read()
+    JOB_CONTENT = job_template.read()
 
-mpich_run_modules = [
-    "singularity/3.9.9",
-    "mpi/mpich/mpich_3.2"
-]
 
-openmpi_run_modules = [
-    "singularity/3.9.9",
-    "mpi/openmpi/4.10/4.10"
-]
+ROCKET_CONTENT: str
+ROCKET_TEMPLATE: Template
+with open("hpc-rocket/rocket.yml.template") as rocket_template:
+    ROCKET_TEMPLATE = Template(rocket_template.read())
 
-node_scale = {
+
+MPICH_RUN_MODULES = ["singularity/3.9.9", "mpi/mpich/mpich_3.2"]
+
+OPENMPI_RUN_MODULES = ["singularity/3.9.9", "mpi/openmpi/4.10/4.10"]
+
+NATIVE_RUN_MODULES = ["mpi/mpich/mpich_3.2", "comp/gcc/9.3.0", "comp/cmake/3.25.0"]
+
+NODE_SCALE = {
     16: {"nx": 4, "ny": 4},
     32: {"nx": 8, "ny": 4},
     64: {"nx": 8, "ny": 8},
 }
 
-srun = "srun --mpi=pmi2"
-mpirun = "mpirun -np {nodes}"
+SRUN = "srun --mpi=pmi2"
+MPIRUN = "mpirun -np {nodes}"
 
-singularity_cmd = "singularity exec {image} /build/bin/laplace {nx} {ny}"
+SINGULARITY_CMD = "singularity exec {bind} {image} /build/bin/laplace {nx} {ny}"
+SINGULARITY_BIND_OPT = "--bind {bind}"
+
+NATIVE_CMD = "build/bin/laplace {nx} {ny}"
+
+MPICH_IMG = "rockylinux9-mpich.sif"
+MPICH_BIND_IMG = "rockylinux9-mpich-bind.sif"
+OPENMPI_IMG = "rockylinux9-openmpi.sif"
+
+MPI_DIR = "/cluster/mpi/mpich"
+
+NATIVE_BUILD_CMD = "mkdir -p build && cd build && cmake .. && make -j4 && cd .."
+
 
 @dataclass
 class Job:
@@ -35,19 +51,34 @@ class Job:
     modules: str
     mpicmd: str
     app: str
+    buildcmd: str = ""
 
-MPICH_IMG = "rockylinux9-mpich.sif"
-OPENMPI_IMG = "rockylinux9-openmpi.sif"
 
 def mpich_job(nodes: int) -> Job:
     return Job(
         nodes=nodes,
         ntasks_per_node=1,
-        workdir=f"mpich_{nodes}",
-        output=f"mpich_{nodes}.out",
-        modules= " ".join(mpich_run_modules),
-        mpicmd=srun,
-        app=singularity_cmd.format(image=MPICH_IMG, **node_scale[nodes]),
+        workdir=f"mpich-{nodes}",
+        output=f"mpich-{nodes}.out",
+        modules=" ".join(MPICH_RUN_MODULES),
+        mpicmd=SRUN,
+        app=SINGULARITY_CMD.format(image=MPICH_IMG, bind="", **NODE_SCALE[nodes]),
+    )
+
+
+def mpich_bind_job(nodes: int) -> Job:
+    return Job(
+        nodes=nodes,
+        ntasks_per_node=1,
+        workdir=f"mpich-bind-{nodes}",
+        output=f"mpich-bind-{nodes}.out",
+        modules=" ".join(MPICH_RUN_MODULES),
+        mpicmd=SRUN,
+        app=SINGULARITY_CMD.format(
+            image=MPICH_BIND_IMG,
+            bind=SINGULARITY_BIND_OPT.format(bind=MPI_DIR),
+            **NODE_SCALE[nodes],
+        ),
     )
 
 
@@ -55,26 +86,54 @@ def openmpi_job(nodes: int) -> Job:
     return Job(
         nodes=nodes,
         ntasks_per_node=1,
-        workdir=f"openmpi_{nodes}",
-        output=f"openmpi_{nodes}.out",
-        modules=" ".join(openmpi_run_modules),
-        mpicmd=mpirun.format(nodes=nodes),
-        app=singularity_cmd.format(image=OPENMPI_IMG, **node_scale[nodes]),
+        workdir=f"openmpi-{nodes}",
+        output=f"openmpi-{nodes}.out",
+        modules=" ".join(OPENMPI_RUN_MODULES),
+        mpicmd=MPIRUN.format(nodes=nodes),
+        app=SINGULARITY_CMD.format(image=OPENMPI_IMG, bind="", **NODE_SCALE[nodes]),
     )
 
+
+def native_job(nodes: int) -> Job:
+    return Job(
+        nodes=nodes,
+        ntasks_per_node=1,
+        workdir=f"native-{nodes}",
+        output=f"native-{nodes}.out",
+        modules=" ".join(NATIVE_RUN_MODULES),
+        mpicmd=SRUN,
+        app=NATIVE_CMD.format(**NODE_SCALE[nodes]),
+        buildcmd=NATIVE_BUILD_CMD,
+    )
+
+
 def format_job_content(job: Job) -> str:
-    return job_content.format(**asdict(job))
+    return JOB_CONTENT.format(**asdict(job))
+
+
+def write_file(name: str, content: str) -> None:
+    with open(name, "w") as file:
+        file.write(content)
 
 
 def write_job_file(name: str, job: Job) -> None:
-    with open(name, "w") as job_file:
-        job_file.write(format_job_content(job))
+    write_file(name, format_job_content(job))
 
 
 if __name__ == "__main__":
-    for nodes, partioning in node_scale.items():
+    for nodes, partioning in NODE_SCALE.items():
         job = mpich_job(nodes)
-        write_job_file(f"mpich_{nodes}.job", job)
+        jobfile = f"slurmjobs/mpich-{nodes}.job"
+        write_job_file(jobfile, job)
+
+        job = mpich_bind_job(nodes)
+        jobfile = f"slurmjobs/mpich-bind-{nodes}.job"
+        write_job_file(jobfile, job)
 
         job = openmpi_job(nodes)
-        write_job_file(f"openmpi_{nodes}.job", job)
+        jobfile = f"slurmjobs/openmpi-{nodes}.job"
+        write_job_file(jobfile, job)
+
+        job = native_job(nodes)
+        jobfile = f"slurmjobs/native-{nodes}.job"
+        write_job_file(jobfile, job)
