8effb56d584e0decad775e873fcc47dd0e7a7518
Sören Peters <dev.peters@icloud.com>
2023-01-17 18:02:20 +0000

    use smaller error in laplace break statement.

--- laplace2d/src/Parameter.h
+++ laplace2d/src/Parameter.h
@@ -15,7 +15,7 @@ struct SimulationParameter
 
     size_t outputInterval = 1000;
 
-    const double error = 1.0e-4;
+    const double error = 1.0e-2;
 
     const char* outputFileName = "results/TemperatureField.avs";
 };
